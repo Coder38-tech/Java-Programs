cfedbb822f67dc5865d89ab0980dd9a4755e3ba4
Coder38-tech <varshneycharu716@gmail.com>
2025-09-08 14:41:11 +0530

    Update Largest_element.java

--- Largest_element.java
+++ Largest_element.java
@@ -12,7 +12,7 @@ public class Largest_element {
 
         }
         
-        int max = 0; 
+        int max = arr[0]; 
         for(int i=0;i<arr.length;i++){
             if(arr[i]>max){
                 max=arr[i];
@@ -22,3 +22,4 @@ public class Largest_element {
     }
     
 }
+
