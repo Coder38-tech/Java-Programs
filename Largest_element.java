9ed5d48b4287d21a9bd68e4b276a0f100b02ec7f
Coder38-tech <varshneycharu716@gmail.com>
2025-09-08 14:28:48 +0530

    Add files via upload

---
+++ Largest_element.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Largest_element {
+    public static void main(String[] args){
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of an array: ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.print("Enter Number:");
+            arr[i]=sc.nextInt();
+
+        }
+        
+        int max = 0; 
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("The largest number is: " + max);
+    }
+    
+}
